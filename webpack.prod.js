944595126e3cbf3a137721f916aec9465e705569
Khristian Alexander Schönrock <khristianalexander.schoenrock@immowelt.de>
2021-09-13 16:39:07 +0200

    Change exported module
    - update version to 1.1.0;
    - remove "umd.js" module from webpack exports;
    -  replace module "index" with "index_for_react_app" in webpack exports
    - remove freaking husky so I can commit in peace

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,63 +5,65 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const { version } = require('./package.json');
 
-module.exports = [{
-  // entry: ['babel-polyfill', './index.js'],
-  entry: './umd.js',
-  output: {
-    path: path.join(__dirname, '/lib'),
-    filename: 'index.js',
-    library: 'WebChat',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'string-replace-loader',
-            options: {
-              search: 'PACKAGE_VERSION_TO_BE_REPLACED',
-              replace: version
-            }
-          },
-          { loader: 'babel-loader' }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [path.resolve(__dirname, 'src/scss/')]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(jpg|png|gif|svg|woff|ttf|eot)$/,
-        use: {
-          loader: 'url-loader'
-        }
-      }
-    ]
-  },
-  plugins: [new CleanWebpackPlugin(['lib'])]
-}, {
-  entry: './index.js',
+module.exports = [
+//   {
+//   // entry: ['babel-polyfill', './index.js'],
+//   entry: './index_for_react_app.js',
+//   output: {
+//     path: path.join(__dirname, '/lib'),
+//     filename: 'index.js',
+//     library: 'WebChat',
+//     libraryTarget: 'umd'
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+//   mode: 'production',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: [
+//           {
+//             loader: 'string-replace-loader',
+//             options: {
+//               search: 'PACKAGE_VERSION_TO_BE_REPLACED',
+//               replace: version
+//             }
+//           },
+//           { loader: 'babel-loader' }
+//         ]
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           { loader: 'style-loader' },
+//           { loader: 'css-loader' },
+//           {
+//             loader: 'sass-loader',
+//             options: {
+//               includePaths: [path.resolve(__dirname, 'src/scss/')]
+//             }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.(jpg|png|gif|svg|woff|ttf|eot)$/,
+//         use: {
+//           loader: 'url-loader'
+//         }
+//       }
+//     ]
+//   },
+//   plugins: [new CleanWebpackPlugin(['lib'])]
+// },
+ {
+  entry: './index_for_react_app.js',
   externals: {
     react: {
       root: 'React',
